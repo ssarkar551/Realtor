964bc0b24525bd1409d363e1077b2965c5bff21b
Subham Sarkar <ssarkar551@gmail.com>
2022-08-08 17:43:13 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async(url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': 'a1ae7aaf7amsha32de1b7c24d67cp1774c8jsn2ea0b86bce7f',
+            'X-RapidAPI-Key': 'YOUR_API_KEY',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
           },
     });
